45a6da19ca24e1d3c5a5f138257a5c416b6d0cd6
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-11-11 18:49:51 -0600

    buttons displaying test passing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+import React from 'react';
+
+import Dashboard from './Dashboard';
+import { render } from 'react-testing-library';
+
+
+test('Dashboard Component Renders Correctly', () => {
+    render(<Dashboard />)
+})
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,15 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react'
+import Display from './Display'
+
+
+test('Display Component Renders Correctly', () => {
+    render(<Display />)
+})
+
+test('display shows both lock/unlock and open/close buttons', () => {
+    const { getByTestId } = render(<Display />);
+    // getByTestId(/open-close-button/i)
+    getByTestId(/lock-unlock-button/i)
+})
