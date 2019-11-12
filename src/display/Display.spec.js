70ba42e833ae2b3b5c88293cbb5a42cc22ceafb4
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-11-11 19:15:46 -0600

    display tests completed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -10,6 +10,36 @@ test('Display Component Renders Correctly', () => {
 
 test('display shows both lock/unlock and open/close buttons', () => {
     const { getByTestId } = render(<Display />);
-    // getByTestId(/open-close-button/i)
+    getByTestId(/open-close-button/i)
     getByTestId(/lock-unlock-button/i)
+})
+
+test('displays Closed if the closed prop is true', () => {
+    const { getByText } = render(<Display closed={true} />)
+    getByText(/closed/i)
+})
+
+test('displays Open if the open prop is true', () => {
+    const { getByText } = render(<Display open={true} />)
+    getByText(/open/i)
+})
+
+test('displays Locked if the locked prop is true', () => {
+    const { getByText } = render(<Display locked={true} />)
+    getByText(/locked/i)
+})
+
+test('displays Unlocked if the locked prop is false', () => {
+    const { getByText } = render(<Display locked={false} />)
+    getByText(/unlocked/i)
+})
+
+test('when locked use the red class', () => {
+    const { getByTestId } = render(<Display locked={true} />)
+    expect(getByTestId(/lock-unlock-button/i).classList.contains('red-led')).toBe(true);
+})
+
+test('when unlocked lock display uses green class', () => {
+    const { getByTestId } = render(<Display locked={false} />)
+    expect(getByTestId(/lock-unlock-button/i).classList.contains('green-led')).toBe(true);
 })
